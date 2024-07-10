318d8e1a9937fadfebc6ed27e9ae29ef124cf4e2
Barath Muthuvel <86292425+BarathMuthuvel@users.noreply.github.com>
2024-07-10 10:05:22 +0530

    first commit

---
+++ String/reverseString.js
@@ -0,0 +1,26 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function reverseString(str) {
+    var reversedStr = '';
+    for (var i = str.length - 1; i >= 0; i--) {
+        reversedStr += str[i];
+    }
+    return reversedStr;
+}
+console.log(reverseString("hello"));
+function runTests() {
+    var input = "hello";
+    // Test with a non-empty string
+    var test1 = reverseString(input);
+    console.log(test1 === "olleh" ? "Test 1 Passed" : "Test 1 Failed");
+    // Test with an empty string
+    var test2 = reverseString(input);
+    console.log(test2 === "" ? "Test 2 Failed" : "Test 2 Passed");
+    // Test with a string containing spaces
+    var test3 = reverseString(input);
+    console.log(test3 === "olleh" ? "Test 3 Passed" : "Test 3 Failed");
+    // Test with a palindrome
+    var test4 = reverseString(input);
+    console.log(test4 === "olleh" ? "Test 4 Passed" : "Test 4 Failed");
+}
+runTests();
